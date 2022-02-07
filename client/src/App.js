6e94c7ff90da270f30e490e0dd48bcdb1e86c661
Hai Nguyen <hhai2105@gmail.com>
2022-02-06 22:53:15 -0500

    add routing to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Switch} from "react-router";
+
+import NavBar from './components/navbar.component';
+import ListPost from './components/list-post.component'
+import EditPost from './components/edit-post.component';
+import CreatePost from './components/create-post.component';
+import CreateUser from './components/create-user.component';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<NavBar />
+			<br/>
+				<Routes>
+					<Route path="/" element={<ListPost/>} />
+					<Route path="/user"  element={<CreateUser/>} />
+					<Route path="/create" element= {<CreatePost/>} />
+					<Route path="/edit/:id" element={<EditPost/>} />
+				</Routes>
+		</Router>
+	);
 }
 
 export default App;
