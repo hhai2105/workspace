0a3371ac0112843c36296167fe60a2e2207165e1
Hai Nguyen <hhai2105@gmail.com>
2022-02-07 23:38:02 -0500

    added catch error to mongoose.connect

--- server/index.js
+++ server/index.js
@@ -1,30 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import windowsRouter from './routes/windows.js';
+import workspacesRouter from './routes/workspaces.js';
 
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/windows', windowsRouter);
+app.use('/workspaces', workspacesRouter);
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true});
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-	console.log("mongoDB database connection established successfully");
-});
-
-const postsRouter = require('./routes/posts');
-const usersRouter = require('./routes/users');
-
- app.use('/posts', postsRouter);
- app.use('/users', usersRouter);
-
+mongoose.connect(process.env.ATLAS_URI, {useNewUrlParser: true})
+	.then(() => console.log("mongoDB database connection established successfully"))
+	.catch(err => console.log("cant connect to mongoDB database, error: " + err));
 
 app.listen(port, () => {
 	console.log('Server is running on port: ' + port);
