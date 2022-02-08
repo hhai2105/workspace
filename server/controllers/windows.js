37127a0e858bb8e5b79633f4e3bd86b1e3105218
Hai Nguyen <hhai2105@gmail.com>
2022-02-07 23:35:30 -0500

    added controllers, move function from routes

---
+++ server/controllers/windows.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import Window from '../models/window.js';
+const router = express.Router();
+
+export const getWindows = async (req,res) =>{
+	try{
+		const window = await Window.find();
+		res.status(200).json(window);
+	}catch(err){
+		res.status(400).json("error: " + err);
+	}
+};
+
+export const getWindow = async (req, res) =>{
+	try{
+		const window = await Window.findById(req.params.id);
+		res.status(200).json(window);
+	}catch(err){
+		res.status(400).json("error: " + err);
+	}
+};
+
+export const addWindows = async (req,res) => {
+	const workspace = req.body.workspace;
+	const type = req.body.type;
+	const data = req.body.data;
+	const newWindow = new Window({username, type, data});
+	try {
+		await newWindow.save();
+		res.status(201).json(newWindow);
+	}catch(err){
+		res.status(400).json("error: " + err);
+	}
+};
+
+
+export const deleteWindows = async (req, res) =>{
+	try{
+		const message = await Window.findByIdAndDelete(req.params.id);
+		res.json('Window deleted');
+	}catch(err){
+		res.status(400).json("error: " + err);
+	}
+};
+
+export const updateWindows = async (req, res) =>{
+	try{
+		window = await Window.findById(req.params.id);
+		window.username = req.body.username;
+		window.type = req.body.type;
+		window.data = req.body.data;
+		message =await window.save();
+		res.json('post updated');
+	}catch(err){
+		res.status(400).json("error: " + err);
+	}
+};
